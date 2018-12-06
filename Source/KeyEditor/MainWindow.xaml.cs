07cdfcfdd042b9f27cb39939886a5173cec95c2b
Catalin Alexandru Nastase <catalin.alexandru.nastase@gmail.com>
2018-12-06 03:06:29 +0200

    - Added AvalonDock.Themes.VS2013 that contains dependency to Xceed.Wpf.AvalonDock to KeyEditor.

--- Source/KeyEditor/MainWindow.xaml.cs
+++ Source/KeyEditor/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Xceed.Wpf.AvalonDock;
+using Xceed.Wpf.AvalonDock.Layout;
+using Xceed.Wpf.AvalonDock.Layout.Serialization;
+
 namespace KeyEditor2
 {
     /// <summary>
@@ -25,9 +30,28 @@ namespace KeyEditor2
             InitializeComponent();
         }
 
-        private void ButtonExit_Click(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
+		private void OnSaveLayout(object sender, RoutedEventArgs e)
+		{
+			string fileName = (sender as MenuItem).Header.ToString();
+			var serializer = new XmlLayoutSerializer(dockManager);
+			using (var stream = new StreamWriter(string.Format(@".\AvalonDock_{0}.config", fileName)))
+				serializer.Serialize(stream);
+		}
+
+		private void OnLoadLayout(object sender, RoutedEventArgs e)
+		{
+			var currentContentsList = dockManager.Layout.Descendents().OfType<LayoutContent>().Where(c => c.ContentId != null).ToArray();
+
+			string fileName = (sender as MenuItem).Header.ToString();
+			var serializer = new XmlLayoutSerializer(dockManager);
+			//serializer.LayoutSerializationCallback += (s, args) =>
+			//    {
+			//        var prevContent = currentContentsList.FirstOrDefault(c => c.ContentId == args.Model.ContentId);
+			//        if (prevContent != null)
+			//            args.Content = prevContent.Content;
+			//    };
+			using (var stream = new StreamReader(string.Format(@".\AvalonDock_{0}.config", fileName)))
+				serializer.Deserialize(stream);
+		}
     }
 }
